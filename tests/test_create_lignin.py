b9b6b66d18a62e9cc31e8e58e0d3201aba2ab644
Heather B Mayes <hmayes@hmayes.com>
2019-11-19 19:24:58 -0700

    updated tests checking json output; do not want the printing of RDKit version to matter to whether the test passes

--- tests/test_create_lignin.py
+++ tests/test_create_lignin.py
@@ -37,6 +37,39 @@ TEST_SMI_OUT_TEMP_DIR = os.path.join(TEMP_DIR, TEST_SMI_BASENAME)
 
 # Data #
 
+# testing pieces of json, skipping parts that refer to version; more thorough testing is beyond scope
+good_json_parts = ['"bonds":[{"bo":2,"atoms":[0,1]},{"atoms":[1,2]},{"bo":2,"atoms":[2,3]},{"atoms":[3,4]},',
+                   '{"atoms":[7,8]},{"atoms":[8,9]},{"atoms":[2,10]},{"atoms":[10,11]},{"atoms":[3,12]},'
+                   '{"bo":2,"atoms":[13,14]},{"atoms":[14,15]},{"bo":2,"atoms":[15,16]},{"atoms":[16,17]},'
+                   '{"bo":2,"atoms":[17,18]},{"atoms":[18,13]},{"atoms":[13,19]},{"atoms":[19,20]},{"atoms":[20,21]},'
+                   '{"atoms":[21,22]},{"atoms":[15,23]},{"atoms":[23,24]},{"atoms":[16,25]},{"atoms":[17,26]},'
+                   '{"atoms":[26,27]},{"bo":2,"atoms":[28,29]},{"atoms":[29,30]},{"bo":2,"atoms":[30,31]},'
+                   '{"atoms":[31,32]},{"bo":2,"atoms":[32,33]},{"atoms":[33,28]},{"atoms":[28,34]},{"atoms":[34,35]},'
+                   '{"atoms":[35,36]},{"atoms":[36,37]},{"atoms":[30,38]},{"atoms":[38,39]},{"atoms":[31,40]},'
+                   '{"atoms":[32,41]},{"atoms":[41,42]},{"bo":2,"atoms":[43,44]},{"atoms":[44,45]},',
+                   '{"atoms":[43,49]},{"bo":2,"atoms":[49,50],"stereo":"trans","stereoAtoms":[43,51]},'
+                   '{"atoms":[50,51]},{"atoms":[51,52]},{"atoms":[45,53]},{"atoms":[53,54]},{"atoms":[46,55]},'
+                   '{"bo":2,"atoms":[56,57]},{"atoms":[57,58]},{"bo":2,"atoms":[58,59]},{"atoms":[59,60]},'
+                   '{"bo":2,"atoms":[60,61]},{"atoms":[61,56]},{"atoms":[56,62]},{"atoms":[62,63]}',
+                   '{"atoms":[64,65]},{"atoms":[58,66]},{"atoms":[66,67]},{"atoms":[59,68]},{"atoms":[60,69]},'
+                   '{"atoms":[69,70]},{"bo":2,"atoms":[71,72]},{"atoms":[72,73]},{"bo":2,"atoms":[73,74]},'
+                   '{"atoms":[74,75]},{"bo":2,"atoms":[75,76]},{"atoms":[76,71]},{"atoms":[71,77]},{"atoms":[77,78]},'
+                   '{"atoms":[78,79]},{"atoms":[79,80]},{"atoms":[73,81]},{"atoms":[81,82]},{"atoms":[74,83]},'
+                   '{"atoms":[75,84]},{"atoms":[84,85]},{"bo":2,"atoms":[86,87]},{"atoms":[87,88]},'
+                   '{"bo":2,"atoms":[88,89]},{"atoms":[89,90]},{"bo":2,"atoms":[90,91]},{"atoms":[91,86]},'
+                   '{"atoms":[86,92]},{"atoms":[92,93]},{"atoms":[93,94]},{"atoms":[94,95]},{"atoms":[88,96]},',
+                   '{"atoms":[104,105]},{"bo":2,"atoms":[105,106]},{"atoms":[106,101]},{"atoms":[101,107]},'
+                   '{"atoms":[107,108]},{"atoms":[108,109]},{"atoms":[109,110]},{"atoms":[103,111]},'
+                   '{"atoms":[111,112]},{"atoms":[104,113]},{"atoms":[105,114]},{"atoms":[114,115]},'
+                   '{"bo":2,"atoms":[116,117]},{"atoms":[117,118]},{"bo":2,"atoms":[118,119]},'
+                   '{"atoms":[119,120]},{"bo":2,"atoms":[120,121]},{"atoms":[121,116]},{"atoms":[116,122]},'
+                   '{"atoms":[122,123]},{"atoms":[123,124]},{"atoms":[124,125]},{"atoms":[118,126]},'
+                   '{"atoms":[126,127]},{"atoms":[119,128]},{"bo":2,"atoms":[129,130]},{"atoms":[130,131]}',
+                   '"atomRings":[[0,5,4,3,2,1],[12,3,4,20,19],[14,15,16,17,18,13],[29,30,31,32,33,28],'
+                   '[72,73,74,75,76,71],[79,78,93,92,80],[87,88,89,90,91,86],[94,93,78,77,95],'
+                   '[117,118,119,120,121,116],[130,131,132,133,134,129],[43,48,47,46,45,44],[55,46,47,63,62],'
+                   '[57,58,59,60,61,56],[102,103,104,105,106,101]]']
+
 
 # Tests #
 
@@ -225,25 +258,35 @@ class TestCreateLigninNormalUse(unittest.TestCase):
 
     def testSaveJSONPathInOutName(self):
         try:
+            silent_remove(DEF_JSON_OUT)
             test_input = ["-r", "10", "-f", "json", "-o", DEF_JSON_OUT]
             main(test_input)
-            self.assertFalse(diff_lines(DEF_JSON_OUT, GOOD_DEF_JSON_OUT))
+            with open(DEF_JSON_OUT, "r") as f:
+                json_str = f.readlines()
+            for json_part in good_json_parts:
+                self.assertTrue(json_part in json_str[0])
         finally:
             silent_remove(DEF_JSON_OUT, disable=DISABLE_REMOVE)
 
     def testSaveJSON(self):
         try:
+            silent_remove(DEF_JSON_OUT)
             test_input = ["-r", "10", "-f", "json", "-d", SUB_DATA_DIR]
             main(test_input)
-            self.assertFalse(diff_lines(DEF_JSON_OUT, GOOD_DEF_JSON_OUT))
+            with open(DEF_JSON_OUT, "r") as f:
+                json_str = f.readlines()
+            self.assertTrue(good_json_parts[0] in json_str[0])
         finally:
             silent_remove(DEF_JSON_OUT, disable=DISABLE_REMOVE)
 
     def testSaveJSONTcl(self):
         try:
+            silent_remove(DEF_JSON_OUT)
             test_input = ["-r", "10", "-f", "json tcl", "-d", SUB_DATA_DIR]
             main(test_input)
-            self.assertFalse(diff_lines(DEF_JSON_OUT, GOOD_DEF_JSON_OUT))
+            with open(DEF_JSON_OUT, "r") as f:
+                json_str = f.readlines()
+            self.assertTrue(good_json_parts[0] in json_str[0])
             self.assertFalse(diff_lines(DEF_TCL_OUT, GOOD_DEF_TCL_OUT))
         finally:
             silent_remove(DEF_JSON_OUT, disable=DISABLE_REMOVE)
@@ -251,13 +294,16 @@ class TestCreateLigninNormalUse(unittest.TestCase):
             pass
 
     def testSaveJSONCommaPNG(self):
-        # This wil use the default image size
+        # Smoke test on png with default image size
         try:
             silent_remove(DEF_PNG_OUT)
+            silent_remove(DEF_JSON_OUT)
             test_input = ["-r", "10", "-f", "json, png", "-d", SUB_DATA_DIR]
             main(test_input)
             self.assertTrue(os.path.isfile(DEF_PNG_OUT))
-            self.assertFalse(diff_lines(DEF_JSON_OUT, GOOD_DEF_JSON_OUT))
+            with open(DEF_JSON_OUT, "r") as f:
+                json_str = f.readlines()
+            self.assertTrue(good_json_parts[0] in json_str[0])
         finally:
             silent_remove(DEF_PNG_OUT, disable=DISABLE_REMOVE)
             silent_remove(DEF_JSON_OUT, disable=DISABLE_REMOVE)
