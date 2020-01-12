2cf8f0b0af9409a691ea888c9c7ea4f717191df0
Heather B Mayes <hmayes@hmayes.com>
2020-01-11 21:00:37 -0700

    all tests now updated, after changing from sets to list to collect possible events for a monomer, as maintained order is required for repeatibility

--- tests/test_create_lignin.py
+++ tests/test_create_lignin.py
@@ -51,8 +51,7 @@ PLOT_BOND_V_SG8_PNG = os.path.join(PLOT_DIR, "bond_dist_v_sg_1e08.png")
 GOOD_JSON_PARTS = ['"bonds":[{"bo":2,"atoms":[0,1]},{"atoms":[1,2]},{"bo":2,"atoms":[2,3]},{"atoms":[3,4]},'
                    '{"bo":2,"atoms":[4,5]},{"atoms":[5,0]},{"atoms":[0,6]},{"atoms":[6,7]},{"atoms":[7,8]},'
                    '{"atoms":[8,9]},{"atoms":[2,10]},{"atoms":[10,11]},{"atoms":[3,12]},{"bo":2,"atoms":[13,14]},'
-                   '{"atoms":[14,15]},{"bo":2,"atoms":[15,16]},{"atoms":[16,17]},{"bo":2,"atoms":[17,18]},'
-                   '{"atoms":[18,13]},{"atoms":[13,19]},{"atoms":[19,20]},{"atoms":[20,21]},{"atoms":[21,22]},'
+                   '{"atoms":[14,15]},{"bo":2,"atoms":[15,16]},{"atoms":[16,17]},{"bo":2,"atoms":[17,18]},',
                    '{"atoms":[15,23]},{"atoms":[23,24]},{"atoms":[16,25]},{"bo":2,"atoms":[26,27]},{"atoms":[27,28]},'
                    '{"bo":2,"atoms":[28,29]},{"atoms":[29,30]},{"bo":2,"atoms":[30,31]},{"atoms":[31,26]},'
                    '{"atoms":[26,32]},{"atoms":[32,33]},{"atoms":[33,34]},{"atoms":[34,35]},{"atoms":[28,36]},'
@@ -65,27 +64,28 @@ GOOD_JSON_PARTS = ['"bonds":[{"bo":2,"atoms":[0,1]},{"atoms":[1,2]},{"bo":2,"ato
                    '{"atoms":[56,64]},{"atoms":[64,65]},{"atoms":[57,66]},{"atoms":[58,67]},{"atoms":[67,68]},'
                    '{"bo":2,"atoms":[69,70]},{"atoms":[70,71]},{"bo":2,"atoms":[71,72]},{"atoms":[72,73]},'
                    '{"bo":2,"atoms":[73,74]},{"atoms":[74,69]},{"atoms":[69,75]},{"atoms":[75,76]},{"atoms":[76,77]},'
-                   '{"atoms":[77,78]},{"atoms":[71,79]},{"atoms":[79,80]},{"atoms":[72,81]},{"bo":2,"atoms":[82,83]},'
-                   '{"atoms":[83,84]},{"bo":2,"atoms":[84,85]},{"atoms":[85,86]},{"bo":2,"atoms":[86,87]},'
-                   '{"atoms":[87,82]},{"atoms":[82,88]},{"atoms":[88,89]},{"atoms":[89,90]},{"atoms":[90,91]},'
-                   '{"atoms":[84,92]},{"atoms":[92,93]},{"atoms":[85,94]},{"bo":2,"atoms":[95,96]},{"atoms":[96,97]},'
-                   '{"bo":2,"atoms":[97,98]},{"atoms":[98,99]},{"bo":2,"atoms":[99,100]},{"atoms":[100,95]},'
-                   '{"atoms":[95,101]},{"atoms":[101,102]},{"atoms":[102,103]},{"atoms":[103,104]},{"atoms":[97,105]},'
-                   '{"atoms":[105,106]},{"atoms":[98,107]},{"bo":2,"atoms":[108,109]},{"atoms":[109,110]},'
-                   '{"bo":2,"atoms":[110,111]},{"atoms":[111,112]},{"bo":2,"atoms":[112,113]},{"atoms":[113,108]},'
-                   '{"atoms":[108,114]},{"atoms":[114,115]},{"atoms":[115,116]},{"atoms":[116,117]},'
-                   '{"atoms":[110,118]},{"atoms":[118,119]},{"atoms":[111,120]},{"bo":2,"atoms":[121,122]},'
-                   '{"atoms":[122,123]},{"bo":2,"atoms":[123,124]},{"atoms":[124,125]},{"bo":2,"atoms":[125,126]},'
-                   '{"atoms":[126,121]},{"atoms":[121,127]},{"atoms":[127,128]},{"atoms":[128,129]},'
-                   '{"atoms":[129,130]},{"atoms":[123,131]},{"atoms":[131,132]},{"atoms":[124,133]},'
-                   '{"atoms":[125,134]},{"atoms":[134,135]},{"atoms":[20,7]},{"atoms":[19,9]},{"atoms":[6,22]},'
-                   '{"atoms":[17,33]},{"atoms":[32,25]},{"atoms":[46,30]},{"atoms":[45,38]},{"atoms":[4,61]},'
-                   '{"atoms":[60,12]},{"atoms":[76,66]},{"atoms":[75,136]},{"atoms":[81,89]},{"atoms":[88,137]},'
-                   '{"atoms":[102,86]},{"atoms":[101,94]},{"atoms":[115,51]},{"atoms":[114,138]},{"atoms":[99,128]},'
-                   '{"atoms":[127,107]}],',
+                   '{"atoms":[77,78]},{"atoms":[71,79]},{"atoms":[79,80]},{"atoms":[72,81]},{"atoms":[73,82]},'
+                   '{"atoms":[82,83]},{"bo":2,"atoms":[84,85]},{"atoms":[85,86]},{"bo":2,"atoms":[86,87]},'
+                   '{"atoms":[87,88]},{"bo":2,"atoms":[88,89]},{"atoms":[89,84]},{"atoms":[84,90]},{"atoms":[90,91]},'
+                   '{"atoms":[91,92]},{"atoms":[92,93]},{"atoms":[86,94]},{"atoms":[94,95]},{"atoms":[87,96]},'
+                   '{"bo":2,"atoms":[97,98]},{"atoms":[98,99]},{"bo":2,"atoms":[99,100]},{"atoms":[100,101]},'
+                   '{"bo":2,"atoms":[101,102]},{"atoms":[102,97]},{"atoms":[97,103]},{"atoms":[103,104]},'
+                   '{"atoms":[104,105]},{"atoms":[105,106]},{"atoms":[99,107]},{"atoms":[107,108]},'
+                   '{"atoms":[100,109]},{"atoms":[101,110]},{"atoms":[110,111]},{"bo":2,"atoms":[112,113]},'
+                   '{"atoms":[113,114]},{"bo":2,"atoms":[114,115]},{"atoms":[115,116]},{"bo":2,"atoms":[116,117]},'
+                   '{"atoms":[117,112]},{"atoms":[112,118]},{"atoms":[118,119]},{"atoms":[119,120]},'
+                   '{"atoms":[120,121]},{"atoms":[114,122]},{"atoms":[122,123]},{"atoms":[115,124]},'
+                   '{"bo":2,"atoms":[125,126]},{"atoms":[126,127]},{"bo":2,"atoms":[127,128]},{"atoms":[128,129]},'
+                   '{"bo":2,"atoms":[129,130]},{"atoms":[130,125]},{"atoms":[125,131]},{"atoms":[131,132]},'
+                   '{"atoms":[132,133]},{"atoms":[133,134]},{"atoms":[127,135]},{"atoms":[135,136]},'
+                   '{"atoms":[128,137]},{"atoms":[129,138]},{"atoms":[138,139]},{"atoms":[17,7]},{"atoms":[6,25]},'
+                   '{"atoms":[33,12]},{"atoms":[32,140]},{"atoms":[30,46]},{"atoms":[45,38]},{"atoms":[61,51]},'
+                   '{"atoms":[60,141]},{"atoms":[66,76]},{"atoms":[75,142]},{"atoms":[91,81]},{"atoms":[90,143]},',
                    '"aromaticAtoms":[0,1,2,3,4,5,13,14,15,16,17,18,26,27,28,29,30,31,39,40,41,42,43,44,54,55,56,57,58,'
-                   '59,69,70,71,72,73,74,82,83,84,85,86,87,95,96,97,98,99,100,108,109,110,111,112,113,121,122,123,124,'
-                   '125,126]']
+                   '59,69,70,71,72,73,74,84,85,86,87,88,89,97,98,99,100,101,102,112,113,114,115,116,117,125,126,127,'
+                   '128,129,130],"aromaticBonds":[0,1,2,3,4,5,13,14,15,16,17,18,26,27,28,29,30,31,39,40,41,42,43,44,'
+                   '54,55,56,57,58,59,69,70,71,72,73,74,84,85,86,87,88,89,97,98,99,100,101,102,112,113,114,115,116,117,'
+                   '125,126,127,128,129,130]']
 
 
 # Tests #
@@ -250,17 +250,15 @@ class TestCreateLigninNormalUse(unittest.TestCase):
         # main(test_input)
         good_chain_summary = "Lignin KMC created 10 monomers, which formed:\n" \
                              "       1 oligomer(s) of chain length 10, with branching coefficient 0.0"
-        good_bond_summary = "composed of the following bond types and number:\n    BO4:    3 " \
-                            "    BB:    1     B5:    5     B1:    0    5O4:    0    AO4:    0     55:    0"
+        good_bond_summary = "composed of the following bond types and number:\n    BO4:    6 " \
+                            "    BB:    0     B5:    3     B1:    0    5O4:    0    AO4:    0     55:    0"
         good_rcf_chain_summary = "Breaking C-O bonds to simulate RCF results in:\n" \
-                                 "       2 monomer(s) (chain length 1)\n       1 trimer(s) (chain length 3)\n" \
-                                 "       1 oligomer(s) of chain length 5, with branching coefficient 0.0"
+                                 "       4 monomer(s) (chain length 1)\n       3 dimer(s) (chain length 2)"
         good_rcf_bond_summary = "with the following remaining bond types and number:\n    BO4:    0     " \
-                                "BB:    1     B5:    5     B1:    0    5O4:    0    AO4:    0     55:    0"
-        good_smiles = "COc1cc(C(O)C(CO)Oc2c(OC)cc(C3Oc4c(OC)cc(C5Oc6c(OC)cc(C7OCC8C(c9cc(OC)c%10c(c9)C(CO)C(c9cc(OC)c" \
-                      "(OC(CO)C(O)c%11ccc(OC(CO)C(O)c%12cc(OC)c%13c(c%12)C(CO)C(c%12cc(OC)c%14c(c%12)C(CO)C(c%12cc(O" \
-                      "C)c([O])c(OC)c%12)O%14)O%13)c(OC)c%11)c(OC)c9)O%10)OCC78)cc6C5CO)cc4C3CO)cc2OC)ccc1[O]"
-
+                                "BB:    0     B5:    3     B1:    0    5O4:    0    AO4:    0     55:    0"
+        good_smiles = "COc1cc(C(O)C(CO)Oc2c(OC)cc(C(O)C(CO)Oc3c(OC)cc(C(O)C(CO)Oc4c(OC)cc(C5Oc6c(OC)cc(C(O)C(CO)Oc7cc" \
+                      "c(C8Oc9c(OC)cc(/C=C/CO)cc9C8CO)cc7OC)cc6C5CO)cc4OC)cc3OC)cc2OC)ccc1OC(CO)C(O)c1cc(OC)c(OC(CO)" \
+                      "C(O)c2cc(OC)c3c(c2)C(CO)C(c2cc(OC)c([O])c(OC)c2)O3)c(OC)c1"
         with capture_stdout(main, test_input) as output:
             self.assertTrue(OPENING_MSG in output)
             self.assertTrue(good_chain_summary in output)
@@ -304,13 +302,13 @@ class TestCreateLigninNormalUse(unittest.TestCase):
         # main(test_input)
         good_chain_summary = "Lignin KMC created 10 monomers, which formed:\n" \
                              "       1 oligomer(s) of chain length 10, with branching coefficient 0.0"
-        good_bond_summary = "composed of the following bond types and number:\n    BO4:    5" \
-                            "     BB:    1     B5:    3     B1:    0    5O4:    0    AO4:    0     55:    0"
+        good_bond_summary = "composed of the following bond types and number:\n    BO4:    7" \
+                            "     BB:    1     B5:    1     B1:    0    5O4:    0    AO4:    0     55:    0"
         good_rcf_chain_summary = "Breaking C-O bonds to simulate RCF results in:\n" \
-                                 "       2 monomer(s) (chain length 1)\n" \
-                                 "       4 dimer(s) (chain length 2)"
+                                 "       6 monomer(s) (chain length 1)\n" \
+                                 "       2 dimer(s) (chain length 2)"
         good_rcf_bond_summary = "with the following remaining bond types and number:\n    BO4:    0    " \
-                                " BB:    1     B5:    3     B1:    0    5O4:    0    AO4:    0     55:    0"
+                                " BB:    1     B5:    1     B1:    0    5O4:    0    AO4:    0     55:    0"
         with capture_stdout(main, test_input) as output:
             self.assertTrue(good_chain_summary in output)
             self.assertTrue(good_bond_summary in output)
@@ -398,20 +396,21 @@ class TestCreateLigninNormalUse(unittest.TestCase):
             pass
 
     def testAltSGRatio(self):
-        test_input = ["-r", "6", "-sg", "2.5", "-a", "1.0"]
+        test_input = ["-r", "8", "-sg", "2.5", "-a", "1.0"]
         # main(test_input)
-        good_smiles = " COc1cc(C2Oc3c(OC)cc(C(O)C(CO)Oc4c(OC)cc(C5Oc6c(OC)cc(C(O)C(CO)Oc7c(OC)cc(C(O)C(CO)Oc8c(OC)cc" \
-                      "(C9Oc%10c(OC)cc(/C=C/CO)cc%10C9CO)cc8OC)cc7OC)cc6C5CO)cc4OC)cc3C2CO)ccc1OC(CO)C(O)c1cc(OC)c2c" \
-                      "(c1)C(CO)C(c1cc(OC)c(OC(CO)C(O)c3cc(OC)c([O])c(OC)c3)c(OC)c1)O2"
+        good_smiles = "COc1cc(C(O)C(CO)Oc2c(OC)cc(C(O)C(CO)Oc3c(OC)cc(C(O)C(CO)Oc4c(OC)cc(C5OCC6C(c7cc(OC)c(OC(CO)" \
+                      "C(O)c8cc(OC)c(OC(CO)C(O)c9cc(OC)c(OC(CO)C(O)c%10cc(OC)c(OC(CO)C(O)c%11cc(OC)c%12c(c%11)C(CO)" \
+                      "C(c%11cc(OC)c([O])c(OC)c%11)O%12)c(OC)c%10)c(OC)c9)c(OC)c8)c(OC)c7)OCC56)cc4OC)cc3OC)cc2OC)" \
+                      "cc(OC)c1[O]"
         with capture_stdout(main, test_input) as output:
             self.assertTrue(good_smiles in output)
 
     def testAltIniMaxMonosSimLen(self):
         test_input = ["-r", "10", "-i", "8", "-m", "12", "-l", "0.02", "-a", "1.0"]
         # main(test_input)
-        good_smiles = "COc1cc(/C=C/CO)ccc1O.COc1cc(C2OCC3C(c4cc(OC)c(Oc5cc(C6Oc7c(OC)cc(/C=C/CO)cc7C6CO)cc(OC)c5[O])" \
-                      "c(Oc5c(OC)cc(C6Oc7c(OC)cc(/C=C/CO)cc7C6CO)cc5OC)c4)OCC23)cc(Oc2c(OC)cc(C3Oc4c(OC)cc(/C=C/CO)c" \
-                      "c4C3CO)cc2OC)c1[O]"
+        good_smiles = "COc1cc(/C=C/CO)cc(OC)c1O.COc1cc(C2OCC3C(c4cc(OC)c([O])c(Oc5c(OC)cc(C6Oc7c(OC)cc(/C=C/CO)cc7C6" \
+                      "CO)cc5OC)c4)OCC23)ccc1O.COc1cc(C2Oc3c(OC)cc(/C=C/CO)cc3C2CO)cc(OC)c1O.COc1cc(C2Oc3c(OC)cc(/C=C" \
+                      "/CO)cc3C2CO)ccc1O"
         with capture_stdout(main, test_input) as output:
             self.assertTrue(good_smiles in output)
 
@@ -436,9 +435,9 @@ class TestDynamics(unittest.TestCase):
             # main(test_input)
             with capture_stdout(main, test_input) as output:
                 self.assertTrue("Lignin KMC created 15 monomers, which formed:\n       "
-                                "1 dimer(s) (chain length 2)\n       "
-                                "1 oligomer(s) of chain length 13, with branching coefficient 0.077" in output)
-                self.assertTrue("BO4:    4     BB:    2     B5:    4     B1:    0    5O4:    3    AO4:    0     "
+                                "5 dimer(s) (chain length 2)\n       "
+                                "1 oligomer(s) of chain length 5, with branching coefficient 0.0" in output)
+                self.assertTrue("BO4:    1     BB:    5     B5:    2     B1:    0    5O4:    1    AO4:    0     "
                                 "55:    0" in output)
             for fname in expected_files:
                 self.assertTrue(os.path.isfile(fname))
@@ -456,9 +455,9 @@ class TestDynamics(unittest.TestCase):
             # main(test_input)
             # testing a piece of output from each of 2 repeats
             with capture_stdout(main, test_input) as output:
-                self.assertTrue("BO4:    7     BB:    2     B5:    5     B1:    0    5O4:    5    AO4:    0     "
+                self.assertTrue("BO4:    2     BB:    7     B5:    2     B1:    0    5O4:    2    AO4:    0     "
                                 "55:    0" in output)
-                self.assertTrue("BO4:    6     BB:    6     B5:    1     B1:    0    5O4:    5    AO4:    0     "
+                self.assertTrue("BO4:    6     BB:    3     B5:    4     B1:    0    5O4:    4    AO4:    0     "
                                 "55:    0" in output)
             for fname in expected_files:
                 self.assertTrue(os.path.isfile(fname))
@@ -476,14 +475,14 @@ class TestDynamics(unittest.TestCase):
             test_input = ["-r", "10", "-i", "3", "-m", "20", "-dy", "-a", "1e6", "-n", "4", "-x"]
             # main(test_input)
             with capture_stdout(main, test_input) as output:
-                self.assertTrue("BO4:    7     BB:    2     B5:    5     B1:    0    5O4:    5    AO4:    0     "
-                                "55:    0" in output)
-                self.assertTrue("BO4:    6     BB:    6     B5:    1     B1:    0    5O4:    5    AO4:    0     "
-                                "55:    0" in output)
-                self.assertTrue("BO4:    6     BB:    2     B5:    5     B1:    0    5O4:    6    AO4:    0     "
-                                "55:    0" in output)
-                self.assertTrue("BO4:    9     BB:    4     B5:    2     B1:    0    5O4:    3    AO4:    0     "
-                                "55:    1" in output)
+                self.assertTrue("BO4:    2     BB:    7     B5:    2     B1:    0    5O4:    2    AO4:    0    "
+                                " 55:    0" in output)
+                self.assertTrue("BO4:    6     BB:    3     B5:    4     B1:    0    5O4:    4    AO4:    0    "
+                                " 55:    0" in output)
+                self.assertTrue("BO4:    5     BB:    2     B5:    6     B1:    0    5O4:    2    AO4:    0    "
+                                " 55:    1" in output)
+                self.assertTrue("BO4:    7     BB:    5     B5:    2     B1:    0    5O4:    5    AO4:    0    "
+                                " 55:    0" in output)
             for fname in expected_files:
                 self.assertTrue(os.path.isfile(fname))
         finally:
@@ -499,7 +498,8 @@ class TestDynamics(unittest.TestCase):
             test_input = ["-r", "10", "-i", "6", "-m", "18", "-a", "1e6", "-dy", "-p", "-d", PLOT_DIR, "-x"]
             # main(test_input)
             with capture_stdout(main, test_input) as output:
-                self.assertTrue("1 oligomer(s) of chain length 18, with branching coefficient 0.111" in output)
+                self.assertTrue("1 trimer(s) (chain length 3)\n       "
+                                "3 oligomer(s) of chain length 5, with branching coefficient 0.0" in output)
             for fname in expected_files:
                 self.assertTrue(os.path.isfile(fname))
         finally:
--- tests/test_data/run_kmc/lignin-kmc-out_good.tcl
+++ tests/test_data/run_kmc/lignin-kmc-out_good.tcl
@@ -7,20 +7,20 @@ segment L {
     residue 3 G
     residue 4 S
     residue 5 S
-    residue 6 G
+    residue 6 S
     residue 7 G
-    residue 8 G
+    residue 8 S
     residue 9 G
     residue 10 S
 }
-patch BB L:1 L:2
-patch B5G L:3 L:2
+patch B5G L:1 L:2
+patch BO4 L:3 L:1
 patch B5G L:4 L:3
-patch B5G L:5 L:1
+patch BO4 L:5 L:4
 patch BO4 L:6 L:5
 patch BO4 L:7 L:6
-patch B5G L:8 L:7
-patch BO4 L:9 L:4
-patch B5G L:10 L:8
+patch BO4 L:8 L:7
+patch BO4 L:9 L:8
+patch B5G L:10 L:9
 regenerate angles dihedrals
 writepsf lignin.psf
